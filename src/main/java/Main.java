4bcc4f1e179b0b74fea72d2b2833fcc2686f3646
retsediv <retsediv1@gmail.com>
2017-09-22 15:09:59 +0300

    Write to the file testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import com.opencsv.CSVWriter;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.io.FileWriter;
+
 
 public class Main {
 
@@ -12,8 +16,20 @@ public class Main {
         String categoryUrl = "https://rozetka.com.ua/all-tv/c80037";
         RozetkaCategoryParser parser = new RozetkaCategoryParser(categoryUrl);
 
-//        ArrayList<HashMap<String, String>> products = this.getProductsOnPage(1);
-//        System.out.println(this.getProductFullDesctiption(products.get(0).get("link")));
+        String path = "data/";
+
+        ArrayList<HashMap<String, String>> products = parser.getAllProducts();
+        for (HashMap<String, String> product : products) {
+
+        }
+
+        String csv = "output.csv";
+        CSVWriter writer = new CSVWriter(new FileWriter(csv));
+        String [] country = "India#China#United States".split("#");
+        writer.writeNext(country);
+
+        writer.close();
+
     }
 
 }
